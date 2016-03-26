63cb1bfbfa3ad99658b536fce8a1b09dab1978ea
Franck Kerbiriou <franck.kerbiriou@gmail.com>
2016-03-26 13:13:07 +0100

    Setup leaflet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ActionController::Base
+class PagesController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
